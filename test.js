f25aff2e176c0efac3ba1716a04089b8b8a72dc3
jeannpaulo <122757486+jeannpaulo@users.noreply.github.com>
2023-03-16 22:03:15 -0300

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-function treinonew () {
-    let treino = document.getElementById("treino").value;
-    let rep = document.getElementById("rep").value;
-    localStorage.setItem(treino, rep);
-    document.getElementById("titulo-treino").innerHTML = treino;
-    document.getElementById("sub-rep").innerHTML = rep;
-}
-
